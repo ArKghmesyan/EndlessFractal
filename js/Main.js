73f452a68664d193607a989218a422eea6264df0
Aram <39464541+ArKghmesyan@users.noreply.github.com>
2022-02-14 21:07:24 +0400

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -80,7 +80,7 @@ function init() {
     // - cubeLR.png (hypercube)
     // - fractalLR.png (fractal)
     // - galaxy.png (default)
-    sprite1 = THREE.ImageUtils.loadTexture("cubeLR.png");
+    sprite1 = THREE.ImageUtils.loadTexture("bm.jpg");
 
     container = document.createElement( 'div' );
     document.body.appendChild(container);
